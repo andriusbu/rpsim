5e0a62093d9c4779081d05abd245e7ceccde30b5
Andrius <andrius@>
2012-05-04 22:39:41 +0300

    SimConfigTest fixed

--- rpsim-core/src/test/java/lt/bumbis/rpsim/core/simconfig/SimConfigTest.java
+++ rpsim-core/src/test/java/lt/bumbis/rpsim/core/simconfig/SimConfigTest.java
@@ -7,18 +7,18 @@ public class SimConfigTest {
 
 	@Test
 	public void testAddGet() {
-		SimConfig conf = new SimConfig("Test");
+		SimConfig conf = new SimConfig("Test", false, false);
 		conf
-			.add(new Distribution("Dist1"))
-			.add(new Distribution("Dist2"))
-			.add(new TokenGenerator("TG1"))
-			.add(new ServiceProcessor("SvcProc1"));
-		assertEquals("Step1", 2, conf.get(Distribution.class).size());
-		assertTrue("Step2", conf.get(Distribution.class).containsKey("Dist2"));
-		assertEquals("Step3", 1, conf.get(TokenGenerator.class).size());
-		assertTrue("Step4", conf.get(TokenGenerator.class).containsKey("TG1"));
-		assertEquals("Step5", 1, conf.get(ServiceProcessor.class).size());
-		assertTrue("Step6", conf.get(ServiceProcessor.class).containsKey("SvcProc1"));
+			.add(new Distribution("Dist1", null, null, false, false))
+			.add(new Distribution("Dist2", null, null, false, false))
+			.add(new TokenGenerator("TG1", null, null, null, false, false))
+			.add(new ServiceProcessor("SvcProc1", 1, null, null, false, false));
+		assertEquals("Step1", 2, conf.getDists().size());
+		assertTrue("Step2", conf.getDists().containsKey("Dist2"));
+		assertEquals("Step3", 1, conf.getTokenGens().size());
+		assertTrue("Step4", conf.getTokenGens().containsKey("TG1"));
+		assertEquals("Step5", 1, conf.getSvcProcs().size());
+		assertTrue("Step6", conf.getSvcProcs().containsKey("SvcProc1"));
 	}
 
 }
