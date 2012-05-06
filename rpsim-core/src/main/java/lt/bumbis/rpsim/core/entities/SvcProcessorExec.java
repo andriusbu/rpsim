1562b9561a2a75d61badb20234b2a8e949638f13
Andrius <andrius@>
2012-05-06 14:13:54 +0300

    Deleted commented lines from events

--- rpsim-core/src/main/java/lt/bumbis/rpsim/core/entities/SvcProcessorExec.java
+++ rpsim-core/src/main/java/lt/bumbis/rpsim/core/entities/SvcProcessorExec.java
@@ -5,17 +5,7 @@ import desmoj.core.simulator.Model;
 
 public class SvcProcessorExec extends Entity {
 	
-//	private SvcReq svcReq;
-
 	public SvcProcessorExec(Model owner, String name, boolean showInTrace) {
         super(owner, name, showInTrace);
     }
-
-//	public SvcReq getSvcReq() {
-//		return svcReq;
-//	}
-
-//	public void setSvcReq(SvcReq svcReq) {
-//		this.svcReq = svcReq;
-//	}
 }
--- rpsim-core/src/main/java/lt/bumbis/rpsim/core/entities/SvcReq.java
+++ rpsim-core/src/main/java/lt/bumbis/rpsim/core/entities/SvcReq.java
@@ -5,18 +5,9 @@ import desmoj.core.simulator.Model;
 
 public class SvcReq extends Entity {
 	
-//	private SvcProcessor svcProcessor;
 	//TODO add handler
 
 	public SvcReq(Model owner, String name, boolean showInTrace) {
         super(owner, name, showInTrace);
 	}
-	
-//	public SvcProcessor getSvcProcessor() {
-//		return svcProcessor;
-//	}
-//
-//	public void setSvcProcessor(SvcProcessor svcProcessor) {
-//		this.svcProcessor = svcProcessor;
-//	}
 }
