1ef6a0adf95244dd7b6787b00e45b8056176788f
Andrius <andrius@>
2012-05-07 22:44:04 +0300

    rpsim-test modele added

---
+++ rpsim-test/src/test/java/lt/bumbis/rpsim/tests/ProcessSimulation.java
@@ -0,0 +1,22 @@
+package lt.bumbis.rpsim.tests;
+
+import static org.junit.Assert.*;
+import lt.bumbis.rpsim.droolsjbpm.ProcessEngineImpl;
+
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.conf.ClockTypeOption;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProcessSimulation {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
