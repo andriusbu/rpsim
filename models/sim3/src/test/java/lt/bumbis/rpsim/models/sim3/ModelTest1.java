27e79359daba1b33b42c6ead4bff5211a53fa065
Andrius <andrius@>
2012-06-03 22:17:42 +0300

    .

--- models/sim3/src/test/java/lt/bumbis/rpsim/models/sim3/ModelTest1.java
+++ models/sim3/src/test/java/lt/bumbis/rpsim/models/sim3/ModelTest1.java
@@ -1,39 +1,47 @@
 package lt.bumbis.rpsim.models.sim3;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import static org.junit.Assert.*;
+
 import java.util.concurrent.TimeUnit;
 
 import lt.bumbis.rpsim.core.SimModel;
 import lt.bumbis.rpsim.core.simconfig.SimConfig;
-import lt.bumbis.rpsim.droolsjbpm.ProcessEngineGuvnor;
+import lt.bumbis.rpsim.droolsjbpm.ProcessEngine;
+import lt.bumbis.rpsim.droolsjbpm.ProcessEngineImpl;
 import lt.bumbis.rpsim.models.sim3.Configuration;
 
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import desmoj.core.simulator.Experiment;
 import desmoj.core.simulator.TimeInstant;
 
 public class ModelTest1 {
+	
+	final static Logger logger = LoggerFactory.getLogger(ProcessEngine.class);
+	
+	private static final String CHANGE_SET = "changeSet.xml";
+	private static final String PROCESS_MODEL = "testProcess.bpmn";
+	private static final String RULES_MODEL = "rule1.drl";
 
 	@Test
-	public void test2() {
+	public void test1() {
 		SimConfig conf = new Configuration();
 		conf.configure();
 		SimModel model = new SimModel(conf);
 
-		ProcessEngineGuvnor procEngine = new ProcessEngineGuvnor(model);
-		try {
-			procEngine.addPackage(new URL("http://localhost:8080/drools-guvnor/org.drools.guvnor.Guvnor/package/lt.bumbis.rpsim.models.sim3/LATEST"));
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-				
+		ProcessEngineImpl procEngine = new ProcessEngineImpl(model);
+		procEngine.addChangeSet(ResourceFactory.newClassPathResource(ModelTest1.CHANGE_SET));
+		procEngine.setEnableRules(true);
+		procEngine.enableLog();
 		model.setProcessEngine(procEngine);
-		Borrower borrower = new Borrower();
-		borrower.setAge(16);
-		borrower.setFirstName("Andrius");
-		borrower.setLasName("Bumblauskas");
 		
 		Experiment exp = new Experiment("Simulation Experiment",true);
 		model.connectToExperiment(exp);
@@ -44,4 +52,16 @@ public class ModelTest1 {
 		exp.report();
 		exp.finish();
 	}
+	
+	@Test
+	public void test2() {
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource(ModelTest1.PROCESS_MODEL), ResourceType.BPMN2);
+		kbuilder.add(ResourceFactory.newClassPathResource(ModelTest1.RULES_MODEL), ResourceType.DRL);
+		if (kbuilder.hasErrors()) {
+			System.out.println(kbuilder.getErrors());
+			assertTrue("Rules has errors", ! kbuilder.hasErrors());
+		}
+	}
 }
+
